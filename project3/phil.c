1d17dc15d01aa69752c5296f7bc55d4a4b87858a
Ege Yosunkaya <egeyosunkaya@gmail.com>
2017-03-24 19:19:29 +0200

    gitignore added

---
+++ project3/phil.c
@@ -0,0 +1,15 @@
+//
+// Created by student on 24.03.2017.
+//
+
+
+
+#include <stdio.h>
+#include <pthread.h>
+
+int main(int argc, char** argv) {
+    printf("%s \n" , "Hello World");
+    printf("%d \n" , argc);
+    printf("%s \n" , argv[1]);
+}
+
