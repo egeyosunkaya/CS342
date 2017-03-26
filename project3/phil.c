e14327000d99d30208b63fb413219f721823cd5d
Ege Yosunkaya <egeyosunkaya@gmail.com>
2017-03-26 14:48:12 +0300

    defined some macros

--- project3/phil.c
+++ project3/phil.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <wait.h>
 #include <semaphore.h>
+#include <time.h>
 
 
 #define PHIL_COUNT 5
@@ -19,7 +20,12 @@
 #define EATING 2
 #define RIGHT (num + 1) % PHIL_COUNT
 #define LEFT  (num + PHIL_COUNT - 1) % PHIL_COUNT
+#define EXPOTENTIAL "expotential"
+#define UNIFORM "uniform"
+#define NORMAL "stdnormal"
 
+#define randnum(min ,max) \
+            (rand() % (int)(((max) + 1) - (min))) + (min))
 
 /*int sem_wait(Semaphore *s) {
     pthread_mutex_lock(s->mutex);
@@ -52,6 +58,8 @@ int sem_init(Semaphore *s){
 sem_t s;
 sem_t locks[PHIL_COUNT];
 int state[PHIL_COUNT];
+int EAT_COUNT;
+
 
 
 void test(int num){
@@ -87,6 +95,7 @@ void put_fork(int num){
 void *foo(void *arg){
     int *i = arg;
     do {
+
         usleep(500); //THINK
 
         take_fork(*i);
@@ -94,12 +103,31 @@ void *foo(void *arg){
         usleep(100); // EAT
 
         put_fork(*i);
+
     }while(1);
 }
 
 int phil_num[PHIL_COUNT];
 
 int main(int argc, char** argv) {
+    srand(time(NULL)); // Seed for random
+
+    EAT_COUNT = atoi(argv[6]);
+    char *distribution = argv[5];
+    int eat_min = atoi(argv[3]);
+    int eat_max = atoi(argv[4]);
+    int think_min = atoi(argv[1]);
+    int think_max = atoi(argv[2]);
+
+
+    printf("%d \n" , EAT_COUNT);
+    printf("%d \n" , eat_min);
+    printf("%d \n" , eat_max);
+    printf("%d \n" , think_min);
+    printf("%d \n" , think_max);
+    printf("%s \n" , distribution);
+
+
     pthread_t thread_list[PHIL_COUNT];
     sem_init(&s , 1 , 1 );
 
@@ -119,18 +147,17 @@ int main(int argc, char** argv) {
 
     }
 
-    do {
+    /*do {
         for (int num = 0; num < PHIL_COUNT; num++) {
             sem_wait(&s);
             int right = RIGHT;
             int left = LEFT;
             if ((state[num] == EATING && state[RIGHT] == EATING) || (state[num] == EATING && state[LEFT] == EATING)) {
                 printf("%s \n" , "We Fucked up guysss!");
-                exit(5);
             }
             sem_post(&s);
         }
-    }while(1);
+    }while(1);*/
 
     int status;
     for(int i = 0; i < PHIL_COUNT; i++){
